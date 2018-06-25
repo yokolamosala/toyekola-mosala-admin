e8481615ac95415738f473e6188405ba0432d228
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2018-06-25 11:35:21 +0000

    angular 6 and prime 6

--- e2e/app.e2e-spec.ts
+++
@@ -1,9 +0,0 @@
-import { AppPage } from './app.po';
-
-describe('Verona App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
-});
---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,9 @@
+import { VeronaPage } from './app.po';
+
+describe('Verona App', () => {
+  let page: VeronaPage;
+
+  beforeEach(() => {
+    page = new VeronaPage();
+  });
+});
