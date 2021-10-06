ac9e635b5b8ec440714616d8bc9edf07f49d6e86
Merve Özçifçi <merveozcifci7@gmail.com>
2021-10-06 23:16:38 +0300

    2. dashboard

--- src/app/demo/view/dashboardanalytics.component.ts
+++ src/app/demo/view/dashboardanalytics.component.ts
@@ -1,16 +1,79 @@
 import {Component, OnInit} from '@angular/core';
-import {EventService} from '../service/eventservice';
-import {SelectItem, MenuItem} from 'primeng/api';
+import {SelectItem} from 'primeng/api';
 import {Product} from '../domain/product';
 import {ProductService} from '../service/productservice';
-import {AppComponent} from '../../app.component';
 
 @Component({
     templateUrl: './dashboardanalytics.component.html',
     styleUrls: ['./tabledemo.scss']
 })
 export class DashboardAnalyticsComponent implements OnInit {
-    ngOnInit() {
 
+    orderWeek: any;
+
+    selectedOrderWeek: any;
+
+    products: Product[];
+
+    productsThisWeek: Product[];
+
+    productsLastWeek: Product[];
+
+    analytics: SelectItem[];
+
+    selectedDrop: SelectItem;
+
+    revenueChart: any;
+
+    revenueOptions: any;
+
+    constructor(private productService: ProductService) {}
+
+    ngOnInit() {
+        this.productService.getProducts().then(data => this.products = data.slice(0, 5));
+        this.productService.getProducts().then(data => this.productsThisWeek = data.slice(0, 5));
+        this.productService.getProductsMixed().then(data => this.productsLastWeek = data.slice(0, 5));
+
+        this.orderWeek = [
+            {name: 'This Week', code: '0'},
+            {name: 'Last Week', code: '1'}
+        ];
+
+        this.analytics = [
+            {label: 'Products', value: 1},
+            {label: 'Sales', value: 2},
+            {label: 'Customers', value: 3},
+        ];
+
+        this.revenueChart = {
+            labels: [
+                'Online',
+                'Retail',
+                'N/A'
+            ],
+            datasets: [{
+                data:  [12, 32, 56],
+                backgroundColor: [  '#1992D4', '#3E4C59', '#E4E7EB'],
+                borderWidth: 0,
+            }]
+        };
+
+        this.revenueOptions = {
+            plugins: {
+                legend: {
+                    display: false,
+                }
+            },
+            responsive: true,
+            cutout: 60
+        };
+    }
+
+    recentSales(event) {
+        if (event.value.code === '0') {
+            this.products = this.productsThisWeek;
+        } else {
+            this.products = this.productsLastWeek;
+        }
     }
 }
