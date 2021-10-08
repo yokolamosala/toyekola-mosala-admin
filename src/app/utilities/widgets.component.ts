7d23e7c54e3341673e7b901c4511c8deddafc14e
Merve Özçifçi <merveozcifci7@gmail.com>
2021-10-08 15:50:59 +0300

    widget page

--- src/app/utilities/widgets.component.ts
+++ src/app/utilities/widgets.component.ts
@@ -1,6 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppComponent} from '../app.component';
 
 @Component({
     templateUrl: './widgets.component.html'
 })
-export class WidgetsComponent {}
+export class WidgetsComponent implements OnInit{
+
+    revenueChart: any;
+
+    revenueOptions: any;
+
+    constructor(public app: AppComponent) {}
+
+    ngOnInit() {
+        this.revenueChart = {
+            labels: [
+                'Online',
+                'Retail',
+                'N/A'
+            ],
+            datasets: [{
+                data:  [12, 32, 56],
+                backgroundColor: [  '#1992D4', '#3E4C59', '#E4E7EB'],
+                borderWidth: 0,
+            }]
+        };
+
+        this.revenueOptions = {
+            plugins: {
+                legend: {
+                    display: false,
+                }
+            },
+            responsive: true,
+            cutout: 60
+        };
+    }
+}
