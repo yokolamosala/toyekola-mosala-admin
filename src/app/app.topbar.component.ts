9add83a2858aabc3956759669641f39380217af0
Merve Özçifçi <merveozcifci7@gmail.com>
2021-10-13 12:11:25 +0300

    search fixed

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { AppComponent} from './app.component';
 import { AppMainComponent} from './app.main.component';
 
@@ -8,5 +8,18 @@ import { AppMainComponent} from './app.main.component';
 })
 export class AppTopBarComponent {
 
+    @ViewChild('input1') inputElement1: ElementRef;
+
+    @ViewChild('input2') inputElement2: ElementRef;
+
     constructor(public app: AppComponent, public appMain: AppMainComponent) {}
+
+    searchFocus(event) {
+        if (this.appMain.search) {
+            setTimeout(() => {
+                this.inputElement1.nativeElement.focus();
+                this.inputElement2.nativeElement.focus();
+            }, 100);
+        }
+    }
 }
