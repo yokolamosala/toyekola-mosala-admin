164b72d638fbeacb8965bb51ac00bc50d330bdff
Şeyma Nur Güllü <67110171+seymanurgullu@users.noreply.github.com>
2022-08-15 15:46:29 +0300

    refactor app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,11 @@
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 import {AppRoutingModule} from './app-routing.module';
-import {BreadcrumbModule} from 'primeng/breadcrumb';
-
-import {ContextMenuModule} from 'primeng/contextmenu';
 import {FullCalendarModule} from '@fullcalendar/angular';
-import {InputSwitchModule} from 'primeng/inputswitch';
-import {MegaMenuModule} from 'primeng/megamenu';
-import {MenuModule} from 'primeng/menu';
-import {MenubarModule} from 'primeng/menubar';
-import {PanelModule} from 'primeng/panel';
-import {PanelMenuModule} from 'primeng/panelmenu';
-import {StepsModule} from 'primeng/steps';
-import {TabMenuModule} from 'primeng/tabmenu';
-import {TabViewModule} from 'primeng/tabview';
-import {TieredMenuModule} from 'primeng/tieredmenu';
 
 // Application Components
 
 import {AppComponent} from './app.component';
-
-
-
-// Demo pages
 
 // Demo servicess
 import {CountryService} from './demo/service/countryservice';
@@ -43,8 +22,6 @@ import {TopbarMenuService} from './layout/app.topbarmenu.service';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { RadioButtonModule } from 'primeng/radiobutton';
-
 import { AppLayoutModule } from './layout/app.layout.module';
 
 FullCalendarModule.registerPlugins([
@@ -57,29 +34,10 @@ FullCalendarModule.registerPlugins([
     imports: [
         AppLayoutModule,
         AppRoutingModule,
-        BrowserModule,
-        FormsModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        BreadcrumbModule,
-        ContextMenuModule,
-        InputSwitchModule,
-        MegaMenuModule,
-        MenuModule,
-        MenubarModule,
-        PanelModule,
-        PanelMenuModule,
-        StepsModule,
-        TabMenuModule,
-        TabViewModule,
-        RadioButtonModule,
-        TieredMenuModule,
 
     ],
     declarations: [
         AppComponent,
-
 
     ],
     providers: [
