62347015874229a7d9c720d001a9adb9c4ab505d
Şeyma Nur Güllü <67110171+seymanurgullu@users.noreply.github.com>
2022-08-16 11:26:03 +0300

    add app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,25 @@ import { PrimeNGConfig } from 'primeng/api';
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export class AppComponent{
+export class AppComponent  implements OnInit{
+    inputStyle = 'outlined';
 
+    ripple: boolean;
 
+    theme = 'indigo';
+
+    layoutColor = 'white';
+
+    colorScheme = 'light';
+
+    menuMode = 'slim';
+
+    constructor(private primengConfig: PrimeNGConfig) {
+    }
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+        this.ripple = true;
+    }
 }
+
