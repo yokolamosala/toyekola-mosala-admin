f6dfbbcb9746721b4a5fd6a17be9193130214c6f
Cagatay Civici <cagatay.civici@gmail.com>
2022-09-15 22:33:49 +0300

    Migrated to css variables

--- src/app/layout/app.layout.component.ts
+++ src/app/layout/app.layout.component.ts
@@ -69,7 +69,8 @@ export class AppLayoutComponent implements OnDestroy {
             'layout-mobile-active': this.layoutService.state.staticMenuMobileActive,
             'layout-static-inactive': this.layoutService.state.staticMenuDesktopInactive && this.layoutService.config.menuMode === 'static',
             'p-input-filled': this.layoutService.config.inputStyle === 'filled',
-            'p-ripple-disabled': !this.layoutService.config.ripple
+            'p-ripple-disabled': !this.layoutService.config.ripple,
+            'layout-white': this.layoutService.config.layoutColor === 'white'
         }
     }
 
