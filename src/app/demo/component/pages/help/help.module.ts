96866e60b2088132383d16dc72fe64dd7ddc7867
Şeyma Nur Güllü <67110171+seymanurgullu@users.noreply.github.com>
2022-09-16 14:19:33 +0300

    added flex to help

--- src/app/demo/component/pages/help/help.module.ts
+++ src/app/demo/component/pages/help/help.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HelpRoutingModule } from './help-routing.module';
 import { HelpComponent } from './help.component';
 import { InputTextModule } from 'primeng/inputtext';
-import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        HelpRoutingModule,
-        InputTextModule,
-        AccordionModule
-    ],
-    declarations: [HelpComponent]
+	imports: [
+		CommonModule,
+		HelpRoutingModule,
+		InputTextModule
+	],
+	declarations: [HelpComponent]
 })
 export class HelpModule { }
