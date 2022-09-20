2ad195381c0fee99525e646c27e350e7727863e1
Şeyma Nur Güllü <67110171+seymanurgullu@users.noreply.github.com>
2022-09-20 11:53:55 +0300

    added faq

--- src/app/demo/component/pages/pages-routing.module.ts
+++ src/app/demo/component/pages/pages-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
     imports: [RouterModule.forChild([
         { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule) },
         { path: 'empty', loadChildren: () => import('./empty/emptydemo.module').then(m => m.EmptyDemoModule) },
+        { path: 'faq', loadChildren: () => import('./faq/faq.module').then(m => m.FaqModule) },
         { path: 'help', loadChildren: () => import('./help/help.module').then(m => m.HelpModule) },
         { path: 'invoice', loadChildren: () => import('./invoice/invoice.module').then(m => m.InvoiceModule) },
         { path: 'timeline', loadChildren: () => import('./timeline/timelinedemo.module').then(m => m.TimelineDemoModule) }
