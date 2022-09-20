5037fdea3ba5cb42eb30b0791fca6debcc78c840
Şeyma Nur Güllü <67110171+seymanurgullu@users.noreply.github.com>
2022-09-20 13:54:51 +0300

    added contact us and about us

--- src/app/demo/component/pages/pages-routing.module.ts
+++ src/app/demo/component/pages/pages-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [RouterModule.forChild([
+        { path: 'aboutus', loadChildren: () => import('./aboutus/aboutus.module').then(m => m.AboutUsModule) },
         { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule) },
+        { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
         { path: 'empty', loadChildren: () => import('./empty/emptydemo.module').then(m => m.EmptyDemoModule) },
         { path: 'faq', loadChildren: () => import('./faq/faq.module').then(m => m.FaqModule) },
         { path: 'help', loadChildren: () => import('./help/help.module').then(m => m.HelpModule) },
