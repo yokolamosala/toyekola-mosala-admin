3607e7795ba9a6dc0491f6c2be6b5f9a41d5dcd8
gucal <gucalaliriza@outlook.com>
2022-10-25 15:24:37 +0300

    Implemented verification page

--- src/app/demo/components/auth/verification/verification.module.ts
+++ src/app/demo/components/auth/verification/verification.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { VerificationRoutingModule } from './verification-routing.module';
 import { VerificationComponent } from './verification.component';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { AppConfigModule } from 'src/app/layout/config/config.module';
 
 @NgModule({
     imports: [
         CommonModule,
         VerificationRoutingModule,
-        FormsModule,
         InputNumberModule,
         ButtonModule,
         RippleModule,
+        AppConfigModule
     ],
     declarations: [VerificationComponent]
 })
