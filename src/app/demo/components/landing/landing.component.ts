d180f4b23132b65a5dfa11e3f5492202db75502f
Cagatay Civici <cagatay.civici@gmail.com>
2022-11-01 14:44:16 +0300

    Fixed background image on dark mode

--- src/app/demo/components/landing/landing.component.ts
+++ src/app/demo/components/landing/landing.component.ts
@@ -1,11 +1,25 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
     templateUrl: './landing.component.html'
 })
-export class LandingComponent { 
+export class LandingComponent {
+
+    constructor(private layoutService: LayoutService) {}
 
     scrollTo(viewChild: HTMLElement) {
         viewChild.scrollIntoView({behavior: 'smooth'});
+    }
+
+    get backgroundStyle(): object {
+        let path = 'assets/demo/images/landing/';
+        let image = this.layoutService.config.colorScheme === 'dark' ? 'line-effect-dark.svg' : 'line-effect.svg'; 
+
+        return {'background-image': 'url(' + path + image + ')'};
+    }
+
+    get colorScheme(): string {
+        return this.layoutService.config.colorScheme;
     }
 }
