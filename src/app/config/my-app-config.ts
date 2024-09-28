4d8a46ea8fa47062c55f4a4999f389b2e0ee3bd1
Daniel Kalala <Daniel@gritprocurement.co.za>
2024-09-28 22:11:50 +0200

    revert to old okta client-id

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc: {
-        clientId: '0oak03qs1d8i6E5nQ5d7', 
-        issuer: 'https://dev-10904060.okta.com/oauth2/default', 
+        clientId: '0oaiy98lldKLzwVQy5d7',
+        issuer: 'https://dev-74279040.okta.com/oauth2/default', 
         redirectUri: window.location.origin + '/login/callback',
         scopes: ['openid', 'profile', 'email'],
         pkce: true
